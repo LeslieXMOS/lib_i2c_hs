fb44f03836f33f3e9541926f003ec24fdf8bf51a
LeslieXMOS <leslielee@xmos.com>
2025-11-14 15:15:00 +0800

    Added misssing data

---
+++ lib_i2c_hs/lib_build_info.cmake
@@ -0,0 +1,10 @@
+set(LIB_NAME lib_i2c_hs)
+set(LIB_VERSION 1.0.0)
+set(LIB_INCLUDES api)
+
+set(LIB_DEPENDENT_MODULES "")
+
+set(LIB_COMPILER_FLAGS -O3
+                       -g)
+
+XMOS_REGISTER_MODULE()
